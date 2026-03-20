557c635d34b3ed1346850ed3aa3353afec7d4176
haakoek <h.e.kristiansen@kjemi.uio.no>
2026-03-20 12:57:38 +0100

    Add Trigonometric approximation of Gaussian envelope.

--- electromagnetic_fields/dipole_fields/envelopes.py
+++ electromagnetic_fields/dipole_fields/envelopes.py
@@ -19,3 +19,36 @@ class SineSquareEnvelope:
             * np.heaviside(dt, 1.0)
             * np.heaviside(self.t_duration - dt, 1.0)
         )
+
+
+class TrigonometricGaussianEnvelope:
+    """
+    The trigonometric envelope for a given n
+        T(t;n,t_c) = cos( pi * (t - tc) / \tau_n)**n,
+    approximates a Gaussian envelope of the form
+        G(t) = exp(-(t - tc)**2 / (2*sigma**2))
+    where
+        \tau_n = pi * sqrt(ln(2)) * sigma / arccos(2^(-1/(2n)))
+    -------------------------------------------------
+    Ref: DOI, 10.1088/0953-4075/42/23/235101
+    """
+
+    def __init__(self, tc, sigma, n):
+        """
+        Parameters
+        ----------
+        tc : float
+            Center of the Gaussian to approximate.
+        sigma : float
+            Width of the Gaussian to approximate.
+        n : int
+            Order of the trigonometric envelope.
+        """
+        self.tc = tc
+        self.n = n
+        self.tau_n = (
+            np.pi * np.sqrt(np.log(2)) * sigma / np.arccos(2 ** (-1 / (2 * n)))
+        )
+
+    def __call__(self, t):
+        return np.cos(np.pi * (t - self.tc) / self.tau_n) ** self.n
